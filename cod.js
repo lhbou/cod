d41362fa021dae74c60ebc06ffe74f23ac6098a3
lhbou <lhassan.bouhou@gmail.com>
2025-01-14 17:22:25 +0100

    Update cod.js

--- cod.js
+++ cod.js
@@ -1,1 +1,9 @@
-alert("okk");
+const OUT_OF_STOCK = ["PROYECTOR-HD"];
+const skuSpan = document.querySelector('span[data-sku]');
+if (OUT_OF_STOCK.includes(skuSpan.getAttribute('data-sku'))) {
+    document.getElementById("codForm").style.display = "none";
+    document.getElementById("buyNow").style.display = "none";
+    document.querySelectorAll(".buyNow").forEach(element => {
+        element.style.display = "none";
+    });
+}
